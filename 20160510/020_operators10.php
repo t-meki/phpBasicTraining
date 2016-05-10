7b21ee4572084ec6b261a344a55418d3eab0e873
t-meki <corbic273@yahoo.co.jp>
2016-05-10 18:11:30 +0900

    merge comitt2

---
+++ 20160510/020_operators10.php
@@ -0,0 +1,17 @@
+<?php
+$x = 6; // 数値
+$y = "6"; // 文字列
+
+// 条件分岐①
+if( $x!=$y ){
+  echo "TRUE<br>";
+} else {
+  echo "FALSE<br>";
+}
+// 条件分岐②
+if( $x!==$y ){
+  echo "TRUE<br>";
+} else {
+  echo "FALSE<br>";
+}
+?>
